5b71143a94d5357fe4e1d30c2757e66cb9bd6fdd
hlyras <hhlyras2011@gmail.com>
2019-05-22 19:59:59 -0300

    product save refactored

--- app/model/factory/product.js
+++ app/model/factory/product.js
@@ -12,7 +12,13 @@ const Product = function(){
 
 Product.save = async (product) => {
 	return new Promise(async (resolve) => {
-		let query = "INSERT INTO lfsdb.products (cod, name, type, color, size) VALUES ('"+product.cod+"', '"+product.name+"','"+product.type+"','"+product.color+"','"+product.size+"');";
+		let query = "INSERT INTO lfsdb.products (cod, name, type, color, size) VALUES ('"
+			+product.cod+"', '"
+			+product.name+"','"
+			+product.type+"','"
+			+product.color+"','"
+			+product.size+"');";
+
 		let user = await db(query);
 		resolve(user);
 	});
