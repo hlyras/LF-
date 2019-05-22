6c05b9369d0db79eafc93f4a8c837542880a4474
hlyras <hhlyras2011@gmail.com>
2019-05-21 23:55:45 -0300

    basic model completed

--- app/controller/user.js
+++ app/controller/user.js
@@ -3,19 +3,15 @@ const userController = {
 		res.render('user/profile', { user: req.user });
 	},
 	verify: (req, res, next) => {
-		if (req.isAuthenticated()){
-			return next();
-		};
+		if (req.isAuthenticated()){ return next() };
 		res.redirect('/user/login');
 	},
 	login: (req, res) => {
-		res.render('user/login', { message: req.flash('loginMessage') });
+		req.session.cookie.maxAge = 1000 * 60 * 5;
+		res.redirect('/');
 	},
 	signup: (req, res) => {
-		res.render('user/signup', { message: req.flash('signupMessage') });
-	},
-	logout: (req, res) => {
-		req.logout();
+		req.session.cookie.maxAge = 1000 * 60 * 5;
 		res.redirect('/');
 	}
 };
--- routes/user.js
+++ routes/user.js
@@ -4,28 +4,15 @@ const passport = require('../config/passport');
 const userController = require('../app/controller/user');
 
 router.get('/', userController.verify, userController.index);
-router.get('/login', userController.login);
-router.get('/signup', userController.signup);
-router.get('/logout', userController.logout);
 
 router.post('/login', passport.authenticate('local-login', { 
 	failureRedirect: '/user/login',
 	failureFlash: true
-}),(req, res) => {
-	if (!req.body.remember) {
-		req.session.cookie.maxAge = 1000 * 60 * 5;
-	} else {
-		req.session.cookie.expires = false;
-	};
-	res.redirect('/');
-});
+}), userController.login);
 
 router.post('/signup', passport.authenticate('local-signup', { 
 	failureRedirect: '/user/signup',
 	failureFlash: true 
-}),(req, res) => {
-	req.session.cookie.maxAge = 1000 * 60 * 5;
-	res.redirect('/');
-});
+}), userController.signup);
 
 module.exports = router;
