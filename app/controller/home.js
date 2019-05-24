6a8a617625c80079f05121a4875493a84cef1265
hlyras <hhlyras2011@gmail.com>
2019-05-24 16:21:49 -0300

    alter signup

--- app/controller/home.js
+++ app/controller/home.js
@@ -11,9 +11,6 @@ const homeController = {
 		res.render('user/login', { message: req.flash('loginMessage') });
 	},
 	signup: async (req, res) => {
-		if(!await userController.verifyAcess(req, res, ['dv','ad'])){
-			return res.redirect('/login');
-		};
 		res.render('user/signup', { message: req.flash('signupMessage') });
 	},
 	logout: (req, res) => {
